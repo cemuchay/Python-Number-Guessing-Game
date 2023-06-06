131a32fa0cb53410bb5cb123d7c43c6960813299
Chizalam <cemuchay@gmail.com>
2023-06-06 10:18:15 +0100

    added check plural function

--- numberguessing.py
+++ numberguessing.py
@@ -1,34 +1,46 @@
 import random
 import math
 
+def check_plural(input_num):
+    if input_num > 0:
+        return 's'
+    else:
+        return ''
+
 # Collecting Integer Inputs
 lower = int(input("\nPlease enter lower bound: "))
 upper = int(input("\nPlease enter upper bound: "))
 
 # Generating pseudo random Int between bounds
-x = random.randint(lower, upper)
+num = random.randint(lower, upper)
 
-print("\nYou have a maximum of ", round(math.log(upper - lower + 1, 2)), "number of guesses")
+guesses = round(math.log(upper - lower + 1, 2))
+
+print("\nYou have a maximum of", guesses, "number of guess" + check_plural(guesses))
 
 # Initializing Count
 count = 0
  
- 
-while count < round(math.log(upper - lower + 1, 2)):
+while count < guesses:
     count += 1
 
-    # Matching Guesss to Random number
+    # Matching Guess to Random number
     guess = int(input('Please Guess a Number: '))
-
-    if guess == x:
-        print('\nCongratulations You did it After', count, "Attempt(s)" , "\n and", round(math.log(upper - lower + 1, 2)) - count, "Guess(es) remaining")
+    
+    attemptsLeft = guesses - count
+    
+    if count == guesses:
+        print(f"\nYour chances are up. The correct number is {num}.")
+        break
+    
+    if guess == num:
+        print(f'\nCongratulations! You did it after {count} attempt{check_plural(count)}. {attemptsLeft} attempt{check_plural(attemptsLeft)} remaining.')
         break
 
-    elif guess < x:
-        print("\nYou guessed too low, Perhaps a Higher Number?","You have ", round(math.log(upper - lower + 1, 2)) - count, "Number of guess(es) Remaining")
+    elif guess < num:
+        print(f"\nYou guessed too low. Perhaps a higher number? You have {attemptsLeft} attempt{check_plural(attemptsLeft)} remaining.")
 
-    elif guess > x:
-        print("\nYou guessed too high, Perhaps a lower Number?", "You have ", round(math.log(upper - lower + 1, 2)) - count, "Number of guess(es) Remaining")
+    elif guess > num:
+        print(f"\nYou guessed too high. Perhaps a lower number? You have {attemptsLeft} attempt{check_plural(attemptsLeft)} remaining.")
 
-    if count == round(math.log(upper - lower + 1, 2)):
-      print("\nYour Chances are up, The correct Number Is", x )
+   
